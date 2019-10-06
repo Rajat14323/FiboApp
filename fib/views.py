9a7fdc01cfa494ea5e7a56df1e83debe82c0268f
rajat14323 <rajatkumarbind@gmail.com>
2019-10-06 06:22:46 +0530

    first commit

---
+++ fib/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import time
+
+# Create your views here.
+def home(request):
+	# return HttpResponse("Hello World")
+	return render(request,'home.html')
+
+def add(request):
+	time_start = time.time()
+
+
+	val1 =request.GET['num1']
+      
+        
+	res= Fibo(int(val1)) 
+
+   
+	return render(request,'result.html',{"result":res,"time":(time.time()-time_start)})
+
+def Fibo(n): 
+    if n<0: 
+        pass
+    # First Fibonacci number is 0 
+    elif n==1: 
+        return 1
+    # Second Fibonacci number is 1 
+    elif n==2: 
+        return 1
+    else: 
+        return Fibo(n-1)+Fibo(n-2) 
